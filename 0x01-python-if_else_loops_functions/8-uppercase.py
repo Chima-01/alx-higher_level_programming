4e0c3404c7639dd89c3693ad5c7896ea9c34e9d4
Chima-01 <divinechima76@gmail.com>
2023-06-07 10:27:32 -0700

    fixed python environment bug from task 7 - 12

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-           i = i.swapcase()
+            i = i.swapcase()
         print("{:s}".format(i), end="")
     print()
