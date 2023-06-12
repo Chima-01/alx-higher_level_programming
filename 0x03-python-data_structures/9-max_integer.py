4ba75ab122f908164003f6f924a9d552ea607940
Chima-01 <divinechima76@gmail.com>
2023-06-12 16:52:47 -0700

    a function that return maximum number in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list:
+        maxi = my_list[0]
+
+        for i in my_list:
+            if maxi < i:
+                maxi = i
+
+        return maxi
