5888e3c8bdd8081f71e8afebf04ff02a6225a882
Chima-01 <divinechima76@gmail.com>
2023-06-12 17:30:18 -0700

    a function that deletes a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if my_list:
+        length = len(my_list)
+
+        if idx < 0 or idx >= length:
+            return my_list
+
+        for i in range(length):
+            if i == idx:
+                del my_list[i]
+
+        return my_list
