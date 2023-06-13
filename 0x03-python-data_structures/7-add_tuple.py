ba672cc62c4b7ccafce3c968d01b6e2415ff732e
Chima-01 <divinechima76@gmail.com>
2023-06-12 20:30:15 -0700

    fixed bug in task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a or tuple_b:
-        _tuple = (0,)
+    _tuple = (0,)
 
-        if len(tuple_a) == 1:
-            tuple_a = tuple_a + _tuple
-        elif len(tuple_a) == 0:
-            tuple_a = _tuple + _tuple
+    if len(tuple_a) == 1:
+        tuple_a = tuple_a + _tuple
+    elif len(tuple_a) == 0:
+        tuple_a = _tuple + _tuple
 
-        if len(tuple_b) == 1:
-            tuple_b = tuple_b + _tuple
-        elif len(tuple_b) == 0:
-            tuple_b = _tuple + _tuple
+    if len(tuple_b) == 1:
+        tuple_b = tuple_b + _tuple
+    elif len(tuple_b) == 0:
+        tuple_b = _tuple + _tuple
 
-        new_tuple_a = tuple_a[0] + tuple_b[0]
-        new_tuple_b = tuple_a[1] + tuple_b[1]
-        new_tuple = (new_tuple_a, new_tuple_b)
+    new_tuple_a = tuple_a[0] + tuple_b[0]
+    new_tuple_b = tuple_a[1] + tuple_b[1]
+    new_tuple = (new_tuple_a, new_tuple_b)
 
-        return new_tuple
+    return new_tuple
