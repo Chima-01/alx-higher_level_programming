0728afdf791753859757f31874aa454ad2e6d68a
Chima-01 <divinechima76@gmail.com>
2023-06-14 15:45:17 -0700

    this function adds to a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# This function replaces or adds key/value in a dictionary.
+
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary:
+        a_dictionary[key] = value
+
+    return a_dictionary
