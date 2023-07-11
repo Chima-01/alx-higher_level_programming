5d38ff08144e2c44f63d35f3d142603d8b6981e6
Chima-01 <divinechima76@gmail.com>
2023-07-11 12:43:30 -0700

    fixed requirement error in task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import os
 """ A function to read from a file"""
 
 
 def read_file(filename=""):
-    """ filename: File to read """
+    """ Args:
+            filename: File to read
+    """
     with open(filename, encoding="utf-8") as f:
         file_v = f.read()
         print(file_v, end="")
