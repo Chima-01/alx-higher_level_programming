39e31619dc49dae75ec56bd621ddce5bb19ca12c
Chima-01 <divinechima76@gmail.com>
2023-07-10 18:45:25 -0700

    raised error if geometry mthod is not declared

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Creted a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """ Geometry """
+
+    def area(self):
+        """ area of geometry """
+        raise Exception("area() is not implemented")
