2ad2db27661bb8adfe9fac204c3d6c2dddbeea2f
Chima-01 <divinechima76@gmail.com>
2023-07-12 16:27:21 -0700

    added doc to the top of file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 """
     This function reads from stdout (cmd line args) then convert
     to a json object in a specified file
 """
 
 
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 if __name__ == "__main__":
     from sys import argv
     """ checks length of argument """
