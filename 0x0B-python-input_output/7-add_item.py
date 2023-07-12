4dcda9ab834b72bbe312d7c206e61bc61448d866
Chima-01 <divinechima76@gmail.com>
2023-07-12 16:01:43 -0700

    fixed import error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 """
@@ -9,6 +8,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 if __name__ == "__main__":
+    from sys import argv
     if len(argv) > 1:
         item = load_from_json_file("add_item.json")
         item.extend(argv[1:])
