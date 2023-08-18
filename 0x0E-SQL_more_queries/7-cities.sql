1e2668d5dc0873c8847de912174913c6e58b927a
Chima-01 <divinechima76@gmail.com>
2023-08-18 08:44:41 -0700

    more queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
--- create a database
+-- create a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,4 @@
 -- list all cities that can be found in database
+SELECT cities.id, cities.name FROM cities, states
+WHERE cities.state_id = states.id AND states.name = "california"
+ORDER BY cities.id ASC;
