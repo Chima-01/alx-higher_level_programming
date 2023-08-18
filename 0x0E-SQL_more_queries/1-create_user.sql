865b0261b88dff36a7bf3e97f5ceb5ca471c729b
Chima-01 <divinechima76@gmail.com>
2023-08-17 17:10:09 -0700

    change styling in task 1 and 0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- create a user on my sql server and
 -- grant all privilege
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost;
