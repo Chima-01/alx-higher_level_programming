57d18396ceba9d52e769a849acf1702e0808f49d
Chima-01 <divinechima76@gmail.com>
2023-08-18 17:49:10 -0700

    fixed error in task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,5 @@ FROM tv_genres
 LEFT JOIN  tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.id IS NOT NULL
 GROUP BY tv_genres.name
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
