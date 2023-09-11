f8d9dd9eca5161e74e75ed2288a0bb4c371d48c8
Chima-01 <divinechima76@gmail.com>
2023-09-11 16:44:23 -0700

    corrected error in file 4, done with task 5

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-arg = process.argv
-console.log(`${arg[2]} is ${arg[3]}`);
+// prints two arguments passed to it
+const args = process.argv;
+console.log(`${args[2]} is ${args[3]}`);
