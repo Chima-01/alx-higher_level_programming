b592e54fc99245269f5c7a9ac88a5b0dbdc8d305
Chima-01 <divinechima76@gmail.com>
2023-09-12 12:01:48 -0700

    fixed egde case in task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments.
 
-const args = process.argv;
+const args = process.argv.slice(2);
 const secondBiggest = findSecondBiggest(args);
 console.log(secondBiggest);
 
 function findSecondBiggest (args) {
-  if (!args || args.length <= 3) {
+  if (!args || args.length <= 1) {
     return 0;
   }
+
   const arr = [];
-  let biggestNum = parseInt(args[2]);
+  let biggestNum = parseInt(args[0]);
   let secondBiggest = biggestNum;
 
   for (let i = 0; i < args.length; i++) {
@@ -18,6 +19,12 @@ function findSecondBiggest (args) {
 
     if (arr[i] > biggestNum) {
       biggestNum = arr[i];
+    }
+  }
+
+  if (arr.length === 2) {
+    if (arr[0] > arr[1]) {
+      return arr[1];
     }
   }
 
