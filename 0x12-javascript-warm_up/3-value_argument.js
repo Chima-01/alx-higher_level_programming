1c8c4783f9f56ffdb6299f9f8d53ae31cc3f10ec
Chima-01 <divinechima76@gmail.com>
2023-09-11 17:00:49 -0700

    fixed error in task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-// prints the first argument passed argv
 const arg = process.argv;
-
-if (arg.length === 2) {
-  console.log('No argument');
-} else if (arg.length > 2) {
+if (arg[2]) {
   console.log(arg[2]);
+} else {
+  console.log('No argument');
 }
