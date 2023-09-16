fc7989ad1bdc705154572b8f5b907015cec2aac8
Chima-01 <divinechima76@gmail.com>
2023-09-16 15:15:01 -0700

    mysql with python

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-""" Connecting to my local database and list staes in that database """
+""" Connecting to my local databases
+list al tables that matches a particular argument"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    mydb = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3],
-        charset="utf8",
-    )
+    if len(sys.argv) == 5:
+        mydb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3],
+            charset="utf8",
+        )
 
-    mycursor = mydb.cursor()
-    sql = 'SELECT * FROM states WHERE name = %s ORDER BY states.id;'
-    mycursor.execute(sql, (sys.argv[4],))
+        mycursor = mydb.cursor()
+        sql = "SELECT * FROM states WHERE name = %s ORDER BY states.id;"
+        mycursor.execute(sql, (sys.argv[4],))
 
-    querry = mycursor.fetchall()
+        querry = mycursor.fetchall()
 
-    for row in querry:
-        print(row)
+        for row in querry:
+            print(row)
 
-    mycursor.close()
-    mydb.close()
+        mycursor.close()
+        mydb.close()
