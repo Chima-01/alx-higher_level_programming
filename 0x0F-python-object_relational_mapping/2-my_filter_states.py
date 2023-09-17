5a8563f76abdde9bcaa8b921f5e3465bbe746c02
Chima-01 <divinechima76@gmail.com>
2023-09-17 06:40:51 -0700

    sql with python

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-""" Connecting to my local databases
-list al tables that matches a particular argument"""
+""" Connecting to my local database and list staes in that database """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    try:
-        if sys.argv[4]:
-            mydb = MySQLdb.connect(
-                host="localhost",
-                port=3306,
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                database=sys.argv[3],
-                charset="utf8",
-            )
+    mydb = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        charset="utf8",
+    )
 
-            mycursor = mydb.cursor()
-            sql = "SELECT * FROM states WHERE name = '{}' ORDER BY \
-                    states.id;".format(
-                sys.argv[4]
-            )
-            mycursor.execute(sql)
+    mycursor = mydb.cursor()
 
-            querry = mycursor.fetchall()
+    sql = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id;".format(
+        sys.argv[4]
+    )
+    mycursor.execute(sql)
 
-            for row in querry:
-                if row[1] == sys.argv[4]:
-                    print(row)
+    querry = mycursor.fetchall()
 
-            mycursor.close()
-            mydb.close()
-    except MySQLdb.Error, e:
-        pass
+    for row in querry:
+        print(row)
+
+    mycursor.close()
+    mydb.close()
