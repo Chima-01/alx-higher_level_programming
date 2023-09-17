d6d466cf223398f9b74c1bfa9739e50d1e7ddb19
Chima-01 <divinechima76@gmail.com>
2023-09-17 12:26:46 -0700

    created a new database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Connecting to my local database and list staes in that database """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        charset="utf8",
+    )
+    mycursor = mydb.cursor()
+
+    sql = "SELECT * FROM cities ORDER BY cities.id;"
+
+    mycursor.execute(sql)
+
+    querry = mycursor.fetchall()
+
+    for row in querry:
+        print(row)
+
+    mycursor.close()
+    mydb.close()
