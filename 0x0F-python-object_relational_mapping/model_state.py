1ae727565481f1f68b07f0ba225af7523b9f56ca
Chima-01 <divinechima76@gmail.com>
2023-09-18 16:55:44 -0700

    documentation to module

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+""" import module for sqlalchemy """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-""" creating a database using sqlalchemy """
 
 db = 'mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa'
 engine = create_engine(db)
@@ -11,6 +11,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
+    """ class to inherit from declarative_base """
+     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
