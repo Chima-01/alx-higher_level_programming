380b5efd35c7bb68f429ee067cb03d08398e413d
chima-01 <divinechima76@gmail.com>
2023-11-15 16:50:59 -0800

    square class inherit from rectanglr

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// square class that inherits from rectangle
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.size; i++) {
+        console.log('C'.repeat(this.size));
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+
+module.exports = Square;
