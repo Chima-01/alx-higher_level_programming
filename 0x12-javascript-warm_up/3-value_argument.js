2a58095679a550f259cb7cd8a5e682f59dd98989
Chima-01 <divinechima76@gmail.com>
2024-01-10 13:27:23 -0800

    fixed error in task 3 and 2

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Write a script that prints a message depending of the number of arguments passed
 
-const arg = process.argv;
+const arg = process.argv[2];
 
-if (arg.length > 2) {
-  console.log(arg[2]);
+if (arg) {
+  console.log(arg);
 } else {
   console.log('No argument');
 }
