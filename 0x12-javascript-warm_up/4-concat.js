cd6d32a311fb162729b5ca08891d9a217a9bae67
Chima-01 <divinechima76@gmail.com>
2024-01-30 08:13:29 -0800

    deleted directories

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-// Write a script that prints two arguments passed to it,
-// in the following format: “ is ”
-const arg = process.argv;
-console.log(`${arg[2]} is ${arg[3]}`);
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// Write a script that prints My number: <first argument converted in integer>
-// if the first argument can be converted to an integer
-const arg = parseInt(process.argv[2]);
-if (!isNaN(arg)) {
-  console.log(`My number: ${arg}`);
-} else {
-  console.log('Not a number');
-}
