e2659b16ca1b368c9b3cd60281627ebbc03e8c37
chima-01 <divinechima76@gmail.com>
2024-02-18 19:26:58 -0800

    first commit for mysqldb task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Intro to mysqldb """
+import MySQLdb
+import sys
+
+username, password, database = sys.argv[1:4]
+conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+)
+
+cur = conn.cursor()
+cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+query_rows = cur.fetchall()
+
+for row in query_rows:
+    print(row)
+
+cur.close()
+conn.close()
