14c6dbcbb1b410207653df24aaf4bb01b6602541
chima-01 <divinechima76@gmail.com>
2024-04-20 12:22:04 -0700

    fixed error in task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,17 +4,12 @@
 const square = require('./5-square');
 
 class Square extends square {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c) {
     if (!c) {
       super.print();
     } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
       }
     }
   }
