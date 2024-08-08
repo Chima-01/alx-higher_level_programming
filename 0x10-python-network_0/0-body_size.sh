5556365a4db92d741e79a4d2f0e1dfe0464316e5
chima-01 <divinechima76@gmail.com>
2024-08-08 09:52:04 -0700

    eddited line of code to task requirement

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # get the content size of a url
-
-if [ "$1" ];
-then
-	curl -s --head "$1" | grep "Content-Length" | cut -d ' ' -f 2
-fi
+curl -s --head "$1" | grep "Content-Length" | cut -d ' ' -f 2
