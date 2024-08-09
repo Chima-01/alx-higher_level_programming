d2271d9485749c614988931e1955911a9b53ddfd
chima-01 <divinechima76@gmail.com>
2024-08-09 15:00:43 -0700

    script to find peak of list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# A function that finds a peak in a list of unsorted integers
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+    peak = list_of_integers[0]
+
+    for num in list_of_integers:
+        if peak < num:
+            peak = num
+
+    return peak
