885a4bcb4e249e761dbd9fac3d566b17d75e7183
chima-01 <divinechima76@gmail.com>
2024-08-10 05:30:01 -0700

    optimize code to improve time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """ A function that finds a peak in a list of unsorted integers """
 
+
 def find_peak(list_of_integers):
     """ check if list is empty """
     if not list_of_integers:
         return None
-    peak = list_of_integers[0]
+    left, right = 0, (len(list_of_integers) - 1)
+    li = list_of_integers
 
-    for num in list_of_integers:
-        if peak < num:
-            peak = num
+    while (left < right):
+        mid = (left + right) // 2
 
-    return peak
+        if li[mid] < li[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return li[left]
