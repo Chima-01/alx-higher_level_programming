fd00d062480167bcd965530fe5a909b194cf7581
chima-01 <divinechima76@gmail.com>
2024-08-11 09:19:13 -0700

    fixed doc error in task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,11 @@
     Python script that takes in a URL, sends a request to the URL and
     displays the value of the X-Request-Id
 """
+
 import sys
 import urllib.request
-""" declare request """
 
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    html = response.info()
-    print(html["X-Request-Id"])
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    html = response.headers
+    print(html.get("X-Request-Id"))
