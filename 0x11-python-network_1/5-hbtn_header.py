6924dd0f02f5e676ec6badd459400cee17317e26
chima-01 <divinechima76@gmail.com>
2024-08-12 04:12:01 -0700

    display specific variable in header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,10 +3,10 @@
     Python script that takes in a URL,
     sends a request to the URL and displays the value
     of the variable X-Request-Id in the response header
-
 """
 import requests
 import sys
+
 
 url = sys.argv[1]
 r = requests.get(url)
